338365755baaa876bd1dc7cb7b83d0e4a0a2a801
Firos Malik Abdillah <144033250+Firosmalik000@users.noreply.github.com>
2025-05-05 10:41:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const userRoute = require('./route/userRoute');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://vonso-final-remidi-qpbl.vercel.app/', credentials: true }));
 
 const dotenv = require('dotenv');
 dotenv.config();
